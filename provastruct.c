b8428cd20d0e9e5cf1662a0aa2bcb1c726ffb9e7
Lorenzo Bardelli <lorybarde@gmail.com>
2023-06-20 23:41:07 +0200

    prova strutture dati

---
+++ provastruct.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define MAX 512
+
+typedef struct{
+    int autonomia;
+}macchina;
+
+typedef struct{
+    macchina parcoAuto [MAX];
+    int distanza;
+}stazione;
+
+int main(){
+    return 0;
+}
