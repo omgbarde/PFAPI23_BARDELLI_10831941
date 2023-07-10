ef36412cac06ab60daee5445406a876c53b2d1a8
Lorenzo Bardelli <lorybarde@gmail.com>
2023-07-11 00:14:10 +0200

    implementazione meccanismo di tokenizzazione dei comandi nel file

--- provafinalev1.c
+++ provafinalev1.c
@@ -4,62 +4,88 @@
 #define MAX 512
 
 typedef struct{
-    int autonomia;
-}macchina;
-
-typedef struct{
-    macchina parcoAuto [MAX];
+    int parcoAuto [MAX];
     int distanza;
-    
 }stazione;
 
 
-void aggiungiStazione(){
+void creaStazione(int dist){
+    printf("%d\n", dist);
+}
+
+void demolisciStazione(int dist){
+    printf("%d\n", dist);
+}
+
+void aggiungiAuto(int dist, int autonomia){
+    printf("%d %d\n", dist ,autonomia);
+}
+
+void rottamaAuto(int dist, int autonomia){
+    printf("%d %d\n", dist ,autonomia);
+}
+
+void pianificaPercorso(int start, int finish){
 
 }
 
-void aggiungiMacchina(){
+void execute(char * curr , int lineSize){
+    char * cmd = strtok(curr," ");
 
-}
-
-void demolisciStazione(){
-
-}
-
-void rottamaAuto(){
-
-}
-
-void pianificaPercorso(){
-
+    if (strcmp(cmd,"aggiungi-stazione")==0){
+        char * dist = strtok(NULL," ");
+        creaStazione(atoi(dist));
+        char * car = strtok(NULL," ");
+        while(car != NULL){
+            aggiungiAuto(atoi(dist), atoi(car));
+            car = strtok(NULL," ");
+        }
+        return;
+    }
+    else if (strcmp(cmd,"demolisci-stazione")==0){
+        char * dist = strtok(NULL," ");
+        demolisciStazione(atoi(dist));
+        return;
+    }
+    else if(strcmp(cmd,"aggiungi-auto")==0){
+        char * dist = strtok(NULL," ");
+        char * arg = strtok(NULL," ");
+        aggiungiAuto(atoi(dist), atoi(arg));
+        return;
+    }
+    else if(strcmp(cmd,"rottama-auto")==0){
+        char * dist = strtok(NULL," ");
+        char * arg = strtok(NULL," ");
+        rottamaAuto(atoi(dist), atoi(arg));
+        return;
+    }
+    else if(strcmp(cmd,"pianifica-percorso")==0){
+        char * start = strtok(NULL," ");
+        char * finish = strtok(NULL," ");
+        rottamaAuto(atoi(start), atoi(finish));
+        return;
+    }
+    else{
+        printf("errore nell' esecuzione del comando '");
+        for(int i = 0; i < lineSize-1; i++){
+            printf("%c",curr[i]);
+        }
+        printf("'\n");
+    }
 }
 
 int main(int argc, char**argv){
-    char* curr = NULL;
+    char * curr = NULL;
     size_t len = 0;
     __ssize_t lineSize = 0;
-    char* cmd;
-    char* token;
 
-    do{
+    while(1){
         lineSize = getline(&curr,&len,stdin);
-        token = strtok(curr," ");
-        if (strcmp(token,"aggiungi-stazione")==0){
-            printf("aggiunta\n");
+        if(lineSize > 0){
+            execute(curr,lineSize);
         }
-        else if (strcmp(token,"demolisci-stazione")==0){
-            printf("demolita\n");
-        }
-        else if(strcmp(token,"aggiungi-auto")==0){
-            printf("aggiunta\n");
-        }
-        else if(strcmp(token,"rottama-auto")==0){
-            printf("rottamata\n");
-        }
-        else if(strcmp(token,"pianifica-percorso")==0){
-            printf("pianificato\n");
-        }
-    }while(lineSize!=-1);
+        else break;
+    }
 
     return 0;
 }
